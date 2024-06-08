ee1345cf44425263c3477ecc687e54e8743a9897
Nikhil Maurya <69952735+nikkhilmaurya@users.noreply.github.com>
2024-06-08 14:26:46 +0530

    Update Challenge1.sol

--- Challenge1.sol
+++ Challenge1.sol
@@ -1,1 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <0.9.0;
+contract AdminRestrict{
+    address public owner;
+    constructor(){
+        owner = msg.sender;
+    } 
 
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only admin can call this function");
+        _;
+    }
+    
+    modifier exceptAdmin(){
+        require(msg.sender != owner, "Except admin everyone can call" );
+        _;
+    }
+
+    function notAdmin() public exceptAdmin view returns (string memory){
+        return "Except Admin anyone can call.";
+    }
+
+    function adminFunction() public onlyOwner view returns (string memory) {
+        return "This function is restricted to the admin.";
+    }
+}
